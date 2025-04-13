9d7d65efd4de429360abc288177e201ad9b2e4c6
Pap Gergő Rajmund <h268307@stud.u-szeged.hu>
2025-04-13 15:19:06 +0200

    Created a way to login/register

--- app/src/main/java/com/example/faketwitter/RegisterActivity.java
+++ app/src/main/java/com/example/faketwitter/RegisterActivity.java
@@ -1,6 +1,5 @@
 package com.example.faketwitter;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,45 +12,65 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG = MainActivity.class.getName();
-    private static final String PREF_KEY = MainActivity.class.getPackage().toString();
-    private static final int SECRET_KEY = 98;
+public class RegisterActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+    private static final String PREF_KEY = RegisterActivity.class.getPackage().toString();
     EditText usernameEditText;
+    EditText emailEditText;
     EditText passwordEditText;
-
+    EditText confirmPasswordEditText;
     private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        usernameEditText = findViewById(R.id.editTextUsername);
-        passwordEditText = findViewById(R.id.editTextPassword);
+        int secretKey = getIntent().getIntExtra("SECRET_KEY", 0);
+        if (secretKey == 99){
+            finish();
+        }
+
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        confirmPasswordEditText = findViewById(R.id.passwordConfirmEditText);
+
         preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
-    }
+        String username = preferences.getString("username", "");
+        String password = preferences.getString("password", "");
 
-    public void login(View view) {
-
-        String username = usernameEditText.getText().toString();
-        String password = passwordEditText.getText().toString();
-
-        Log.i(LOG_TAG, "Bejelentkezett: "+username + ", jelszó: "+password);
+        usernameEditText.setText(username);
+        passwordEditText.setText(password);
+        Log.i(LOG_TAG, "onCreate");
     }
 
     public void register(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        intent.putExtra("SECRET_KEY", SECRET_KEY);
-        startActivity(intent);
 
+
+        String username = usernameEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+
+        if (!password.equals(confirmPassword)){
+            Log.e(LOG_TAG, "A jelszavak nem egyeznek!");
+            return;
+        }
+
+        Log.i(LOG_TAG, "Regisztrált: "+username + ", email: "+email);
+    }
+
+    public void cancel(View view) {
+        finish();
     }
 
     @Override
@@ -75,10 +94,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        super.onPause();
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString("username", usernameEditText.getText().toString());
+        editor.putString("email", emailEditText.getText().toString());
         editor.putString("password", passwordEditText.getText().toString());
+        editor.putString("confirmPassword", confirmPasswordEditText.getText().toString());
         editor.apply();
+        Log.i(LOG_TAG, "onPause");
         Log.i(LOG_TAG, "onPause");
     }
 
